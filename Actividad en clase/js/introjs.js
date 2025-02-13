0ddc6958828ee817dc55344cd6b19ac7373773eb
Elmay05 <fewdinosaur@gmail.com>
2025-02-13 10:39:03 -0600

    Mensajes y alertas en JS

---
+++ Actividad en clase/js/introjs.js
@@ -0,0 +1,14 @@
+//consola (log, info, warn, error, assert)
+console.log("Hola mundo wookie!"); //no es necesario el ";" pero se ponen por diciplina
+console.info("Aquí andamos aprendiendo");
+console.warn("Te vamos a coger si sigues hakeando");
+console.error("grrrrrrrrr");
+console.assert(1 == 1); //compara valores sin especificar
+console.assert(1 === 1);
+console.assert(1 == "1");
+console.assert(1 === "1"); //comparacion estricta, toma en cuenta el tipo de dato
+console.assert(1 == true);
+console.assert(1 === true);
+
+//
+//si no tiene memoria devuelve 0
