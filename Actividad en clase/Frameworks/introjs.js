221d8caea73766a09bebd9f5d8888b54b4ec498c
Elmay05 <fewdinosaur@gmail.com>
2025-02-17 10:18:24 -0600

    refactor estract function

--- Actividad en clase/Frameworks/introjs.js
+++ Actividad en clase/Frameworks/introjs.js
@@ -1,6 +1,14 @@
+//document es para jalar los datos del html
+//jala los elementos por medio de un id del html
 const boton = document.getElementById("boton_imagen");
 console.log(boton);
 
-boton.onclick = () => {
-  console.log("click");
+const poner_imagen_1 = () => {
+  const span_imagen = document.getElementById("imagen_chewy");
+  //innerHTML propiedad para modificar html interno del DOM, SIRVE PARA USAR EL LENGUAJE DE HTML
+  span_imagen.innerHTML = `<img alt="cheewbaca guapo"
+          src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhgVp24TlNmDm2CaFYbBHLT0tAWoIRrUJPujBnNieQANku2V2XtR8kRLmyhn-_B-gxZmSNHHONU3XKwaj8-1HowsVWn-I3fmP3-7TPVoXKWra36PRJQGwr4lvukjNRARDlVw2dB30um6TFP/s1600/movies_star_wars_bts_pics_13.jpg"
+        >`;
 };
+//SE UTILIZAN `` para escribir lo que va el 1 "linea" en varias (ejemplo anterior de la img)
+boton.onclick = poner_imagen_1;
