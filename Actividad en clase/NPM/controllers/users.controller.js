50d8cec81eee2cd63954ea1dfa8bade84cf74e9f
Elmay05 <fewdinosaur@gmail.com>
2025-03-10 11:06:23 -0600

    Lectura Metodología para diseñar casos de pruebas a partir de casos de uso

--- Actividad en clase/NPM/controllers/users.controller.js
+++ Actividad en clase/NPM/controllers/users.controller.js
@@ -1,14 +1,58 @@
+const Usuario = require('../models/usuario.model');
+const bcrypt = require('bcryptjs');
+
+exports.get_signup = (request, response, next) => {
+    response.render('login.ejs', {
+        isLoggedIn: request.session.isLoggedIn || false,
+        username: request.session.username || '',
+        isNew: true,
+        csrfToken: request.csrfToken(),
+    });
+};
+
+exports.post_signup = (request, response, next) => {
+    const nuevo_usuario = new 
+        Usuario(request.body.username, request.body.password);
+
+    nuevo_usuario.save().then(() => {
+        response.redirect('/users/login');
+    }).catch((error) => {
+        console.log(error);
+    });
+};
+
 exports.get_login = (request, response, next) => {
     response.render('login.ejs', {
         isLoggedIn: request.session.isLoggedIn || false,
         username: request.session.username || '',
+        isNew: false,
+        csrfToken: request.csrfToken(),
     });
 };
 
 exports.post_login = (request, response, next) => {
-    request.session.username = request.body.username;
-    request.session.isLoggedIn = true;
-    response.redirect('/personajes');
+    Usuario.fetchOne(request.body.username).then(([rows, fieldData]) => {
+        if (rows.length > 0) {
+            bcrypt.compare(request.body.password, rows[0].password)
+                .then((doMatch) => {
+                    if (doMatch) {
+                        request.session.username = rows[0].username;
+                        request.session.isLoggedIn = true;
+                        return request.session.save(err => {
+                            response.redirect('/personajes');
+                        });
+                    } else {
+                        response.redirect('/users/login');
+                    }
+            }).catch((error) => {
+                console.log(error);
+            });
+        } else {
+            response.redirect('/users/login');
+        }
+    }).catch((error) => {
+        console.log(error);
+    });
 };
 
 exports.get_logout = (request, response, next) => {
